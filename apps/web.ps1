38464c11abc489ca9780396d501d25fe668e5ae8
Ryan Thomas <ryan.thomas@willistowerswatson.com>
2024-05-29 23:09:37 -0400

    Added docker, terminal, and web

---
+++ apps/web.ps1
@@ -0,0 +1,28 @@
+
+Write-Host "Downloading Firefox"
+# Define the URL for the latest version of Firefox
+$url = "https://download.mozilla.org/?product=firefox-latest&os=win&lang=en-US"
+
+# Define the path where the installer will be saved
+$outfile = "$env:TEMP\FirefoxSetup.exe"
+
+# Download the installer
+Invoke-WebRequest -Uri $url -OutFile $outfile
+
+Write-Host "Installing Firefox"
+# Run the installer
+Start-Process -FilePath $outfile -Args "/S" -Wait -PassThru
+
+Write-Host "Downloading Postman"
+# Define the URL for the latest version of Postman
+$url = "https://dl.pstmn.io/download/latest/win64"
+
+# Define the path where the installer will be saved
+$outfile = "$env:TEMP\PostmanSetup.exe"
+
+# Download the installer
+Invoke-WebRequest -Uri $url -OutFile $outfile
+
+Write-Host "Installing Postman"
+# Run the installer
+Start-Process -FilePath $outfile -Wait -PassThru
